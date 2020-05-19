5f11969bbf522d05b0f6098fe420a3ea3aea923f
Alexey_Lisenko <detoner777@gmail.com>
2020-05-19 17:10:31 +0300

    функции\перегрузка функции

--- script.js
+++ script.js
@@ -1,4 +1,29 @@
 "use strict";
-console.log('Hello TypeScript');
-console.log('Good By');
+function a(x, y) {
+    return x + y;
+}
+var b = function (x, y) { return x + y; };
+// /Сигнатура:
+var c;
+c = function (x, y) {
+    return x + y;
+};
+///void если не будем использовать return
+function d(x, y) {
+    console.log(x + y);
+}
+/// функция которая никогда не завершиться(бесконечный цикл)
+function e(x) {
+    throw new Error(x);
+}
+function g(x, y) {
+    if (typeof x === 'number') {
+        return x + y;
+    }
+    else {
+        return x + " " + y;
+    }
+}
+console.log(g(1, 2));
+console.log(g('sds', 2));
 //# sourceMappingURL=script.js.map
--- script.ts
+++ script.ts
@@ -1,15 +1,56 @@
-let a: number = 1;
+function a(x: number ,y: number): number {
+	return x + y;
+}
 
-let b: string ='Hello';
+let b = (x: number, y: number): number => x + y;
 
-let c: boolean = true;
+// /Сигнатура:
 
-let d: null = null;
+let c: (x: number, y: number) => number;
 
-let e: undefined = undefined;
+c = function (x: number, y: number): number {
+	return x + y;
+}
+///void если не будем использовать return
 
-let f:{text: string} = {text: "Hello"};
+function d(x: number, y: number): void {
+	console.log(x +y)
+}
+/// функция которая никогда не завершиться(бесконечный цикл)
 
-let g: any; 
+function e (x: string): never {
+	throw new Error(x)
+}
+
+// /Перегрузка функций:
+
+// function f(x: any, y: any): any {
+// 	if (typeof x === 'number') {
+// 		return x + y
+// 	} else {
+// 		return `${x} ${y}`
+// 	}
+// 	}
+
+function g(x: number, y: number):number;
+function g(x: string, y: number):string;
+function g(x: any, y: any): any {
+	if (typeof x === 'number') {
+		return x + y
+	} else {
+		return `${x} ${y}`
+	}
+	}
+
+	console.log(g(1, 2));
+	console.log(g('sds', 2));
+
+	
+		
+
+
+
+
+
 
 
