76c19fa9ce52e082cef05b1e25fe597af0cdd7d4
Alexey_Lisenko <detoner777@gmail.com>
2020-05-19 16:46:30 +0300

    ts types

--- script.ts
+++ script.ts
@@ -1,4 +1,15 @@
-console.log('Hello TypeScript');
-console.log('Good By');
+let a: number = 1;
+
+let b: string ='Hello';
+
+let c: boolean = true;
+
+let d: null = null;
+
+let e: undefined = undefined;
+
+let f:{text: string} = {text: "Hello"};
+
+let g: any; 
 
 
