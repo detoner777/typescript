61904413b56a8045994b2c0ed29123a7da371338
Alexey_Lisenko <detoner777@gmail.com>
2020-05-25 09:41:06 +0300

    пространство имен

--- script.js
+++ script.js
@@ -1,77 +1,78 @@
 "use strict";
-// interface IPerson {
-//     name?: string;
-//     age: number;
-//     sayHello(): string;
-// }
-var __extends = (this && this.__extends) || (function () {
-    var extendStatics = function (d, b) {
-        extendStatics = Object.setPrototypeOf ||
-            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
-        return extendStatics(d, b);
-    };
-    return function (d, b) {
-        extendStatics(d, b);
-        function __() { this.constructor = d; }
-        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-    };
-})();
-var Person = /** @class */ (function () {
-    function Person(name, age) {
-        this.name = '';
-        this.age = 0;
-        this.name = name;
-        this.age = age;
+var __classPrivateFieldSet = (this && this.__classPrivateFieldSet) || function (receiver, privateMap, value) {
+    if (!privateMap.has(receiver)) {
+        throw new TypeError("attempted to set private field on non-instance");
     }
-    Person.prototype.sayHello = function () {
-        return "\u041F\u0440\u0438\u0432\u0435\u0442, \u043C\u0435\u043D\u044F \u0437\u043E\u0432\u0443\u0442 " + this.name;
-    };
-    return Person;
-}());
-var Student = /** @class */ (function (_super) {
-    __extends(Student, _super);
-    function Student(name, age, group, course) {
-        var _this = _super.call(this, name, age) || this;
-        _this.group = '';
-        _this.course = 0;
-        _this.group = group;
-        _this.course = course;
-        return _this;
+    privateMap.set(receiver, value);
+    return value;
+};
+var __classPrivateFieldGet = (this && this.__classPrivateFieldGet) || function (receiver, privateMap) {
+    if (!privateMap.has(receiver)) {
+        throw new TypeError("attempted to get private field on non-instance");
     }
-    Student.prototype.sayHello = function () {
-        // return `привет я ${this.name} из группы ${this.group}`;
-        var text = _super.prototype.sayHello.call(this);
-        return text + " \u042F \u0438 \u0433\u0440\u0443\u043F\u043F\u044B " + this.group;
-    };
-    Student.prototype.description = function () {
-        return "\n            \u0418\u043C\u044F: " + this.name + "\n            \u0412\u043E\u0437\u0432\u0440\u0430\u0441\u0442: " + this.age + "\n            \u0413\u0440\u0443\u043F\u043F\u0430: " + this.group + "\n            \u041A\u0443\u0440\u0441: " + this.course + "\n        ";
-    };
-    return Student;
-}(Person));
-var Teacher = /** @class */ (function (_super) {
-    __extends(Teacher, _super);
-    function Teacher(name, age, disciplines) {
-        var _this = _super.call(this, name, age) || this;
-        _this.disciplines = [];
-        _this.disciplines = disciplines;
-        return _this;
+    return privateMap.get(receiver);
+};
+var Utils;
+(function (Utils) {
+    function log(text, color, fontSize) {
+        if (fontSize) {
+            console.log(`%c${text}`, `color: ${color}; font-size: ${fontSize}px`);
+        }
+        else {
+            console.log(`%c${text}`, `color: ${color}`);
+        }
     }
-    Teacher.prototype.description = function () {
-        return "\n            \u0418\u043C\u044F: " + this.name + "\n            \u0412\u043E\u0437\u0432\u0440\u0430\u0441\u0442: " + this.age + "\n            \u0414\u0438\u0441\u0446\u0438\u043F\u043B\u0438\u043D\u044B: " + this.disciplines + "\n            ";
-    };
-    return Teacher;
-}(Person));
-// const ira: Person = new Person('Ира', 18);
-// console.log(ira);
-var denis = new Student('Денис', 25, 'A1', 1);
-console.log(denis);
-// приведение типов
-var newDenis = denis;
-console.log(newDenis);
-var liza = new Student('Лиза', 20, 'A2', 2);
-console.log(liza);
-var igor = new Teacher('Igor', 23, ['TypeScript', 'React Native']);
-console.log(igor);
-// https://www.youtube.com/watch?v=0zmMHXoUzRo
+    Utils.log = log;
+    log('Text inside', '#d9534f', 40);
+})(Utils || (Utils = {}));
+Utils.log('Text outside', '#d9534f');
+var Animals;
+(function (Animals) {
+    class Animal {
+        constructor(name) {
+            this.name = name;
+        }
+    }
+    Animals.Animal = Animal;
+    let Pets;
+    (function (Pets) {
+        class Cat extends Animal {
+            say() {
+                Utils.log(`${this.name}: myauu`, `#96ceb4`, 20);
+            }
+        }
+        Pets.Cat = Cat;
+        class Dog extends Animal {
+            say() {
+                Utils.log(`${this.name}: gav`, `#ffceb4`, 30);
+            }
+        }
+        Pets.Dog = Dog;
+    })(Pets = Animals.Pets || (Animals.Pets = {}));
+})(Animals || (Animals = {}));
+(function (Animals) {
+    var _text;
+    class Tiger extends Animals.Animal {
+        constructor(name, text) {
+            super(name);
+            _text.set(this, void 0);
+            this.text = text;
+            __classPrivateFieldSet(this, _text, text);
+        }
+        say() {
+            Utils.log(`${this.name}: ${this.text} ${__classPrivateFieldGet(this, _text)}`, '#ffad60', 60);
+        }
+    }
+    _text = new WeakMap();
+    Animals.Tiger = Tiger;
+})(Animals || (Animals = {}));
+const tiger = new Animals.Tiger('Tigr', 'rrr');
+// console.log(tiger.text);
+// console.log(tiger.#text);
+tiger.say();
+const cat = new Animals.Pets.Cat('Barsik');
+cat.say();
+const dog = new Animals.Pets.Dog('Sharik');
+dog.say();
+// https://www.youtube.com/watch?v=jKN-VqWQdlY
 //# sourceMappingURL=script.js.map
--- script.ts
+++ script.ts
@@ -1,79 +1,68 @@
-abstract class Person {
-    public name: string = '';
-    // private name: string = '';
-    // protected name: string = '';
-    // age: number = 0;
-    public readonly age: number = 0;
+namespace Utils {
+    export function log(text: string, color: string): void;
+    export function log(text: string, color: string, fontSize: number): void;
 
-    constructor(name: string, age: number) {
-        this.name = name;
-        this.age = age;
+    export function log(text: string, color: string, fontSize?: number) {
+        if (fontSize) {
+            console.log(`%c${text}`, `color: ${color}; font-size: ${fontSize}px`);
+        } else {
+            console.log(`%c${text}`, `color: ${color}`);
+        }
     }
 
-    // constructor(public name: string, public age: number) {}
-
-    sayHello(): string {
-        return `Привет, меня зовут ${this.name}`;
-    }
-
-    abstract description(): string;
+    log('Text inside', '#d9534f', 40);
 }
 
-class Student extends Person {
-    group: string = '';
-    course: number = 0;
+Utils.log('Text outside', '#d9534f');
 
-    constructor(name: string, age: number, group: string, course: number) {
-        super(name, age);
-        this.group = group;
-        this.course = course;
+namespace Animals {
+    export abstract class Animal {
+        protected name: string;
+
+        constructor(name: string) {
+            this.name = name;
+        }
+        abstract say(): void;
     }
-    sayHello(): string {
-        // return `привет я ${this.name} из группы ${this.group}`;
-        const text = super.sayHello();
-        return `${text} Я и группы ${this.group}`;
-    }
-    description(): string {
-        return `
-            Имя: ${this.name}
-            Возвраст: ${this.age}
-            Группа: ${this.group}
-            Курс: ${this.course}
-        `;
+    export namespace Pets {
+        export class Cat extends Animal {
+            say() {
+                Utils.log(`${this.name}: myauu`, `#96ceb4`, 20);
+            }
+        }
+        export class Dog extends Animal {
+            say() {
+                Utils.log(`${this.name}: gav`, `#ffceb4`, 30);
+            }
+        }
     }
 }
 
-class Teacher extends Person {
-    disciplines: string[] = [];
+namespace Animals {
+    export class Tiger extends Animals.Animal {
+        private text: string;
+        #text: string;
 
-    constructor(name: string, age: number, disciplines: string[]) {
-        super(name, age);
-        this.disciplines = disciplines;
-    }
-    description(): string {
-        return `
-            Имя: ${this.name}
-            Возвраст: ${this.age}
-            Дисциплины: ${this.disciplines}
-            `;
+        constructor(name: string, text: string) {
+            super(name);
+            this.text = text;
+            this.#text = text;
+        }
+        say() {
+            Utils.log(`${this.name}: ${this.text} ${this.#text}`, '#ffad60', 60);
+        }
     }
 }
 
-// const ira: Person = new Person('Ира', 18);
-// console.log(ira);
+const tiger = new Animals.Tiger('Tigr', 'rrr');
+// console.log(tiger.text);
+// console.log(tiger.#text);
+tiger.say();
 
-const denis: Student = new Student('Денис', 25, 'A1', 1);
-console.log(denis);
-console.log(denis.name);
+const cat = new Animals.Pets.Cat('Barsik');
+cat.say();
 
-// приведение типов
-const newDenis: Person = <Person>denis;
-console.log(newDenis);
+const dog = new Animals.Pets.Dog('Sharik');
+dog.say();
 
-const liza: Person = new Student('Лиза', 20, 'A2', 2);
-console.log(liza);
-
-const igor: Teacher = new Teacher('Igor', 23, ['TypeScript', 'React Native']);
-console.log(igor);
-
-// https://www.youtube.com/watch?v=X492_WJgQhI
+// https://www.youtube.com/watch?v=jKN-VqWQdlY
